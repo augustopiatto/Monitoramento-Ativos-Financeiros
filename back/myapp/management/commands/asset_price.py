a56ebf13658dada0f0d88cef6b3771bff1868196
Augusto <ampiatto@hotmail.com>
2023-11-28 18:14:24 -0300

    atualiza os ativos no banco com valor atual e last updated

--- back/myapp/management/commands/asset_price.py
+++ back/myapp/management/commands/asset_price.py
@@ -2,6 +2,11 @@ from myapp.models import Asset
 from django.core.management.base import BaseCommand
 from django.db.models import Q, F, ExpressionWrapper, fields, Case, When, Value, Func
 from django.utils import timezone
+from myapp.services.external_asset_svc import asset_list
+
+
+BASE_URL = "https://brapi.dev/api/"
+now = timezone.now()
 
 
 class Command(BaseCommand):
@@ -11,27 +16,56 @@ class Command(BaseCommand):
         pass
 
     def handle(self, *args, **options):
-        class DurationToMinutes(Func):
-            function = 'EXTRACT'
-            template = "%(function)s(EPOCH FROM %(expressions)s) / 60"
+        # Fluxo:
 
-        # vou ter q pegar todos o assets do meu banco que tiverem o now() - last_updated >= periodicity
+        # Vou ter q pegar todos o assets do meu banco que tiverem o now() - last_updated >= periodicity
         # e buscar na api externa do brapi (talvez ver alguma diferente, que possa passar os nomes
         # e retornar só as que preciso)
-        now = timezone.now()
 
-        assets_to_update = Asset.objects.annotate(
-            time_diff=Case(
-                When(last_updated__isnull=False, then=ExpressionWrapper(
-                    now - F('last_updated'),
-                    output_field=fields.DurationField()
-                )),
-                default=Value(None),
-                output_field=fields.DurationField()
-            )
-        ).annotate(
-            time_diff_minutes=DurationToMinutes('time_diff')
-        ).filter(Q(last_updated__isnull=True) | Q(periodicity__lte=F('time_diff_minutes')))
+        # Atualizar os cur_value e last_updated destes assets
 
-        print(assets_to_update)
+        # Comparar os cur_value com os limites de tunel max_value e min_value e
+        # enviar e-mail de template "COMPRAR" ou "VENDER"
+
+        assets_to_update = get_assets_to_update()
+        update_assets(assets_to_update)
+
+        print("assets_to_update", assets_to_update)
         print("Comando 'asset_price' rodou!")
+
+
+def get_assets_to_update():
+    class DurationToMinutes(Func):
+        function = "EXTRACT"
+        template = "%(function)s(EPOCH FROM %(expressions)s) / 60"
+
+    assets_to_update = Asset.objects.annotate(
+        time_diff=Case(
+            When(last_updated__isnull=False, then=ExpressionWrapper(
+                now - F("last_updated"),
+                output_field=fields.DurationField()
+            )),
+            default=Value(None),
+            output_field=fields.DurationField()
+        )
+    ).annotate(
+        time_diff_minutes=DurationToMinutes("time_diff")
+    ).filter(Q(last_updated__isnull=True) | Q(periodicity__lte=F("time_diff_minutes")))
+
+    return assets_to_update
+
+
+def update_assets(assets):
+    all_assets = asset_list()
+
+    assets_to_update = []
+    for asset_obj in assets:
+        asset_api = next((asset for asset in all_assets if asset["stock"] == asset_obj.name), None)
+
+        if asset_api:
+            #: API retorna um numero com ponto separando as casas decimais. O Python lida com isso.
+            asset_obj.cur_value = asset_api["close"]
+            asset_obj.last_updated = now
+            assets_to_update.append(asset_obj)
+
+    Asset.objects.bulk_update(assets_to_update, ["cur_value", "last_updated"], batch_size=500)
--- back/myapp/services/external_asset_svc.py
+++ back/myapp/services/external_asset_svc.py
@@ -4,17 +4,34 @@ from django.core.exceptions import ValidationError
 
 
 BASE_URL = "https://brapi.dev/api/"
+GENERIC_ERROR_MESSAGE = "A requisição ao Brapi não foi bem sucedida"
 
 
 def asset_list():
     try:
         params = {
             "sortBy": "name",
-            'sortOrder': 'asc',
+            "sortOrder": "asc",
+            "token": ASSETS_EXTERNAL_URL_API_KEY
         }
-        url = BASE_URL + 'quote/list?token=%s' % ASSETS_EXTERNAL_URL_API_KEY
+        url = BASE_URL + "quote/list"
         response = requests.get(url, params=params)
     except:
-        raise ValidationError("A requisição ao Brapi não foi bem sucedida")
+        raise ValidationError(GENERIC_ERROR_MESSAGE)
     data = response.json()
+
     return data["stocks"]
+
+# Tentei fazer buscando somente os ativos que eu queria, mas o Brapi só tem essa opção no pago
+# def filtered_asset_list(asset_names_list):
+#     try:
+#         params = {
+#             "token": ASSETS_EXTERNAL_URL_API_KEY
+#         }
+#         url = BASE_URL + "quote/%s" % asset_names_list
+#         response = requests.get(url, params=params)
+#     except:
+#         raise ValidationError(GENERIC_ERROR_MESSAGE)
+#     data = response.json()
+
+#     return data["stocks"]
